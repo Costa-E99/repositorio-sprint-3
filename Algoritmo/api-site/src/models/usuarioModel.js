d849e73e5e923b821f3d974ddabe261663a171f1
gcomartins <gcomartins@gmail.com>
2021-11-13 17:44:59 -0300

    Implementação da API de Login e Cadastro

---
+++ Algoritmo/api-site/src/models/usuarioModel.js
@@ -0,0 +1,45 @@
+var database = require("../database/config")
+
+function listar() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+        SELECT * FROM usuario;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function entrar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function cadastrar_empresa(nome, email, cnpj, responsavel) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, cnpj, responsavel);
+    var instrucao = `
+        INSERT INTO empresa (nome, email, cnpj, responsavel) VALUES ('${nome}', '${email}', '${cnpj}', '${responsavel}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function cadastrar_usuario(responsavel, cpf, setor, cargo,  email, senha, nome) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",responsavel, cpf, setor, cargo, email, senha, nome);
+    var instrucao = `
+        INSERT INTO usuario (nome, cpf, setor, cargo,  email, senha, fkempresa) (select '${responsavel}', '${cpf}', '${setor}', '${cargo}', '${email}', '${senha}', idempresa
+        from empresa where nome = "${nome}");
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    entrar,
+    cadastrar_empresa,
+    cadastrar_usuario,
+    listar,
+};
