c1dd18af121fe6992cb5417d7d775eb22c0e83a6
Costa-E99 <eduardo.costa@bandtec.com.br>
2021-12-08 18:33:11 -0300

    arranjos na azure

--- Algoritmo/api-site/src/models/medidaModel.js
+++ Algoritmo/api-site/src/models/medidaModel.js
@@ -1,24 +1,24 @@
 var database = require("../database/config");
 
 function buscarUltimasMedidas(idAquario, limite_linhas) {
-    instrucaoSql = `select 
+    instrucaoSql = `select top 20
                         tempatual,
                         DATE_FORMAT(datahora,'%H:%i:%s') as momento_grafico
                     from dados
                     where fkcaminhao = ${idAquario}
-                    order by iddados desc limit 20`;
+                    order by iddados desc `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function buscarMedidasEmTempoReal(idAquario) {
-    instrucaoSql = `select 
+    instrucaoSql = `select top 1
                         tempatual,
                         DATE_FORMAT(datahora,'%H:%i:%s') as momento_grafico
                         from dados
                         join caminhao on fkcaminhao = idcaminhao 
                         where fkcaminhao = ${idAquario} 
-                    order by iddados desc limit 1;`;
+                    order by iddados desc;`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
