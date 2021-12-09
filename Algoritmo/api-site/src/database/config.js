a659fd9e7105eb6e5e96b69e6711f04b25a51c2d
gcomartins <gcomartins@gmail.com>
2021-12-09 16:24:52 -0300

    Volta para o Workbench

--- Algoritmo/api-site/src/database/config.js
+++ Algoritmo/api-site/src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
   host: "localhost",
   user: "root",
   database: "sprint2",
-  password: "macaco10"
+  password: "Chinelinho@12"
 };
 
 function executar(instrucao) {
--- Algoritmo/api-site/src/models/medidaModel.js
+++ Algoritmo/api-site/src/models/medidaModel.js
@@ -1,24 +1,24 @@
 var database = require("../database/config");
 
 function buscarUltimasMedidas(idAquario, limite_linhas) {
-    instrucaoSql = `select top 20
+    instrucaoSql = `select
                         tempatual,
                         DATE_FORMAT(datahora,'%H:%i:%s') as momento_grafico
                     from dados
                     where fkcaminhao = ${idAquario}
-                    order by iddados desc `;
+                    order by iddados desc limit 20; `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function buscarMedidasEmTempoReal(idAquario) {
-    instrucaoSql = `select top 1
+    instrucaoSql = `select
                         tempatual,
                         DATE_FORMAT(datahora,'%H:%i:%s') as momento_grafico
                         from dados
                         join caminhao on fkcaminhao = idcaminhao 
                         where fkcaminhao = ${idAquario} 
-                    order by iddados desc;`;
+                    order by iddados desc limit 1;`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
